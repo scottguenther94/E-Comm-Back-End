18b889f4d6fcae658ef559bd65b08fb0b97e1aec
Southern Comfort <scottguenther94@gmail.com>
2021-11-11 20:11:59 -0500

    Created repo, uploaded starting code

---
+++ Develop/routes/api/tag-routes.js
@@ -0,0 +1,68 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  Tag.findAll({
+    include: [
+      {
+        model: Product,
+        through: ProductTag,
+      },
+    ],
+  })
+  .then((tags)=> res.status(200).json(tags))
+  .catch((err)=> res.status(500).json(err));
+  // find all tags
+  // be sure to include its associated Product data
+});
+
+router.get('/:id', (req, res) => {
+  Tag.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+        through: ProductTag
+      },
+    ],
+  })
+  .then((tags)=> res.status(200).json(tags))
+  .catch((err)=> res.status(500).json(err));
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+});
+
+router.post('/', (req, res) => {
+  Tag.create(req.body)
+  .then((tags)=> res.status(200).json(tags))
+  .catch((err)=> res.status(500).json(err));
+  // create a new tag
+});
+
+router.put('/:id', (req, res) => {
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((tags)=> res.status(200).json(tags))
+  .catch((err)=> res.status(500).json(err));
+  // update a tag's name by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((tags)=> res.status(200).json(tags))
+  .catch((err)=> res.status(500).json(err));
+  // delete on tag by its `id` value
+});
+
+module.exports = router;
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# E-Comm-Back-End
+# Object Relational Mapping(ORM): E-Commerce Back End
+
+### Link to walk-through video: https://drive.google.com/file/d/13KHLhfQ8bNXpVEN_AwM6bnGlhsrH2EAR/view
+
+### Link to Github Repo: https://github.com/karlagoo/ORM
+
+## Description
+```
+an Express.js API that utilizes Sequelize to interact with a MySQL database
+```
+
+
+## User Story
+As a manager at an internet retail company I want a back end for my e-commerce website that uses the latest technologies SO THAT my company can compete with other e-commerce companies
+
+
+## Usage
+* Application allows user to view all Products, Categories, and Tags databases or search by ID
+* Users are able to create, delete, update Products, Categories, and Tags databases
+
+
+## Screenshots
+<img width="1140" alt="Screen Shot 2021-09-11 at 10 26 05 PM" src="https://user-images.githubusercontent.com/84356242/132971046-ca9fd914-2b6d-4eef-8588-6bb61718fe9f.png">
+<img width="1146" alt="Screen Shot 2021-09-11 at 10 25 20 PM" src="https://user-images.githubusercontent.com/84356242/132971051-2c11dba7-9776-4112-8c4c-43fed45f460b.png">
+
+## Contributor:
+Karla Goo © All Rights Reserved

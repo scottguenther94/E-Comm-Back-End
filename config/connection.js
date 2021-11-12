48eed761b93f97bb9ea5b95c4ce4e95ea671ca2c
Southern Comfort <scottguenther94@gmail.com>
2021-11-11 23:23:57 -0500

    Updated connection

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,12 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+const sequelize = new Sequelize({
+    dialect: "mysql",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: +process.env.DATABASE_PORT || 3306
+});
 
-module.exports = sequelize;
+module.exports = sequelize; 
